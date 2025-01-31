aa36ba1a3a18cb27d419a3b1e80d5edc4983bc68
mubina20090207 <abduraxmonovamubina02@gmail.com>
2025-01-30 22:09:23 -0800

    Initial commit

--- shop/models.py
+++ shop/models.py
@@ -21,6 +21,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     name = models.CharField(max_length=100)

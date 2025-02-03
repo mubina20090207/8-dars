b28f1b820e9758ce4bf3dffe120a49758d52ae04
mubina20090207 <abduraxmonovamubina02@gmail.com>
2025-02-03 13:45:17 -0800

    Initial commit

--- shop/models.py
+++ shop/models.py
@@ -31,7 +31,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-
+        
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
     image = models.ImageField(upload_to='products/')
